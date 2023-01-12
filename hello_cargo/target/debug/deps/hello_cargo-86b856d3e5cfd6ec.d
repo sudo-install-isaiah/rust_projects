3b546d86a6f1ff926be5dfb1297b3e30b4104ca0
zae <isaiah.iu@hotmail.com>
2023-01-12 15:14:07 -0500

    Use cargo new to build rust project 'hello_cargo'

---
+++ hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec.d
@@ -0,0 +1,5 @@
+/home/zae/rust/hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec: src/main.rs
+
+/home/zae/rust/hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/zae/rust/hello_cargo/target/debug/hello_cargo: /home/zae/rust/hello_cargo/src/main.rs
